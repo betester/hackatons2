e930fcf5ad8efee249057fc2cf029bcce79af3ec
betester <ikramzulkarnaen02@gmail.com>
2024-07-27 14:58:03 +0700

    clustering sir?

--- backend/go.mod
+++ backend/go.mod
@@ -28,6 +28,7 @@ require (
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/sys v0.22.0 // indirect
 	golang.org/x/text v0.16.0 // indirect
+	gonum.org/v1/gonum v0.15.0 // indirect
 	google.golang.org/protobuf v1.34.2 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- backend/service/main.go
+++ backend/service/main.go
@@ -1,27 +1,39 @@
 package service
 
-import(
-    "hackatons2/backend/data"
+import (
+	"hackatons2/backend/data"
+	"time"
 )
 
-var database map[int]data.AccidentReport = make(map[int]data.AccidentReport) 
-var currentId int = 0 
+var reportDatabase map[int]data.AccidentReport = make(map[int]data.AccidentReport) 
+var currentAccidentReportId int = 0
 
-const REPORT_BASE_PATH string = "accident_report" 
+const (
+    REPORT_BASE_PATH string = "accident_report" 
+    SUMMARY_BASE_PATH string = "accident_summary"
+    MAX_RADIUS float64 = 1000
+)
 
 func AddAccidentReport(report data.AccidentReport) {
-    report.Id = currentId
-    currentId += 1
-    database[report.Id] = report;
+
+    report.Id = currentAccidentReportId
+    report.CreatedTimeStamp = time.Now()
+    currentAccidentReportId += 1
+    reportDatabase[report.Id] = report;
+
+    // TODO: when adding on this place emit the gabagoo to another function which will decide the severity
 }
 
 func GetAccidentReport() []data.AccidentReport {
 
-    response := make([]data.AccidentReport, len(database))
+    response := make([]data.AccidentReport, len(reportDatabase))
+    locations := make([][2]float64, len(response))
 
-    for _, value := range database {
-        response = append(response, value)
+    for i := range response {
+        locations[i][0] = response[i].Location.Latitude
+        locations[i][1] = response[i].Location.Longitude
     }
-
+    
+    // clusteredLocs := geo.Dbscan(locations, MAX_RADIUS)
     return response
 }

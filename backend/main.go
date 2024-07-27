bbe7b212ed83bed339213788073400c1407f5572
betester <ikramzulkarnaen02@gmail.com>
2024-07-27 19:07:03 +0700

    add cors handler

--- backend/main.go
+++ backend/main.go
@@ -10,6 +10,25 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+func CORSMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+
+		c.Header("Access-Control-Allow-Origin", "*")
+		c.Header("Access-Control-Allow-Methods", "GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Access-Control-Allow-Headers", "authorization, origin, content-type, accept")
+		c.Header("Allow", "HEAD,GET,POST,PUT,PATCH,DELETE,OPTIONS")
+		c.Header("Content-Type", "application/json")
+
+		if c.Request.Method == "OPTIONS" {
+			print("masuk ke opstion")
+			c.AbortWithStatus(204)
+			return
+		}
+
+		c.Next()
+	}
+}
+
 func main() {
     r := gin.Default()
     summaryDuration := time.NewTicker(10 * time.Second)
@@ -22,6 +41,8 @@ func main() {
         }
         }
     }()
+
+    r.Use(CORSMiddleware())
 
     r.GET("/ping", func(c *gin.Context) {
         c.JSON(200, gin.H{
